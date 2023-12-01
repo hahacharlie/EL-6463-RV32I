da74c0ef4aa2bceb385937301c0367736b4bb3c3
Raleigh-Wang <152100389+Raleigh-Wang@users.noreply.github.com>
2023-11-30 20:20:29 -0500

    Add files via upload

---
+++ AHD_FINAL.srcs/sources_1/new/data.v
@@ -0,0 +1,145 @@
+`timescale 1ns / 1ps
+
+module Data(
+    input logic         clk,
+    input logic         rst,
+    input logic [2:0]   w_mode,
+    input logic [2:0]   r_mode,
+    input logic [31:0]  addr_in,
+    input logic [31:0]  din,
+    input logic [9:0]   opc_in,
+    output logic [31:0] dout
+);
+
+    logic [7:0]     dmem1[0:1023];
+    logic [7:0]     dmem2[0:1023];
+    logic [7:0]     dmem3[0:1023];
+    logic [7:0]     dmem4[0:1023];
+    
+    initial begin
+        for (int i = 0; i < 1024; i++) begin
+            dmem1[i] = 0;
+            dmem2[i] = 0;
+            dmem3[i] = 0;
+            dmem4[i] = 0;
+        end
+    end
+
+    logic [31:0] row;
+    logic [31:0] addr_dmem;
+    logic [31:0] index_dmem;
+    logic [31:0] index;
+    logic [31:0] dO;
+    logic [31:0] d_t;
+    logic        dout_se;
+    logic [9:0]  opcode;
+    logic [6:0]  func7;
+    logic [2:0]  func3;
+    
+    assign addr_dmem = addr_in & (~32'h80000000);
+    assign row = {2'b0, addr_dmem[31:2]};
+    assign index_dmem = addr_in & 32'h00000003;
+    assign index = index_dmem;
+    
+    assign opcode = opc_in;
+    assign dout_se = (opcode == 10'b0000000011 || opcode == 10'b0010000011);
+    
+    assign func7 = opc_in[6:0];
+    assign func3 = opc_in[9:7];
+    
+    always_ff @(negedge rst or posedge clk) begin
+        if (!rst) begin
+            dO <= 0;
+        end else begin
+            if (addr_dmem < 4096) begin
+                case (r_mode)
+                    3'b001: dO <= {24'h000000, dmem1[row]};
+                    3'b011: dO <= {16'h0000, dmem2[row], dmem1[row]};
+                    3'b111: dO <= {dmem4[row], dmem3[row], dmem2[row], dmem1[row]};
+                    default: dO <= 0;
+                endcase
+            end else begin
+                dO <= 0;
+            end
+        end
+    end
+    
+    always_comb begin
+        if (dout_se) begin
+            case (func3)
+                3'b000: d_t = {{24{dO[7]}}, dO[7:0]};
+                3'b001: d_t = {{24{dO[15]}}, dO[15:0]};
+                default: d_t = dO;
+            endcase
+        end else begin
+            case (func3)
+                3'b100: d_t = {{24{dO[7]}}, dO[7:0]};
+                3'b101: d_t = {{24{dO[15]}}, dO[15:0]};
+                default: d_t = dO;
+            endcase
+        end
+    end
+
+    assign dout = d_t;
+
+    always_ff @(posedge clk) begin
+        if (addr_dmem < 4096) begin
+            if (w_mode == 3'b001) begin
+                case (index)
+                    0: dmem1[row] <= din[7:0];
+                    1: dmem2[row] <= din[7:0];
+                    2: dmem3[row] <= din[7:0];
+                    3: dmem4[row] <= din[7:0];
+                endcase
+            end else if (w_mode == 3'b011) begin
+                case (index)
+                    0: begin
+                            dmem1[row] <= din[7:0];
+                            dmem2[row] <= din[15:8];
+                        end
+                    1: begin
+                            dmem2[row] <= din[7:0];
+                            dmem3[row] <= din[15:8];
+                        end
+                    2: begin
+                            dmem3[row] <= din[7:0];
+                            dmem4[row] <= din[15:8];
+                        end
+                    3: begin
+                            dmem4[row] <= din[7:0];
+                            dmem1[row + 1] <= din[15:8];
+                        end
+                endcase
+            end else if (w_mode == 3'b111) begin
+                case (index)
+                    0: begin
+                            dmem1[row] <= din[7:0];
+                            dmem2[row] <= din[15:8];
+                            dmem3[row] <= din[23:16];
+                            dmem4[row] <= din[31:24];
+                        end
+                    1: begin
+                            dmem2[row] <= din[7:0];
+                            dmem3[row] <= din[15:8];
+                            dmem4[row] <= din[23:16];
+                            dmem1[row + 1] <= din[31:24];
+                        end
+                    2: begin
+                            dmem3[row] <= din[7:0];
+                            dmem4[row] <= din[15:8];
+                            dmem1[row + 1] <= din[23:16];
+                            dmem2[row + 1] <= din[31:24];
+                        end
+                    3: begin
+                            dmem4[row] <= din[7:0];
+                            dmem1[row + 1] <= din[15:8];
+                            dmem2[row + 1] <= din[23:16];
+                            dmem3[row + 1] <= din[31:24];
+                        end
+                endcase
+            end
+        end
+    end
+
+endmodule
+
--- README.md
+++ README.md
@@ -1,11 +1,63 @@
-# GY-6463-ahd_project 
+# Instruction Memory  
 
-## Charlie
-PC and Control Unit
+## Module Description
+* The Instruction Memory module is implemented in System Verilog.
+* It is designed to simulate a read-only memory (ROM) segment that stores the processor's instruction set.
+* The module fetches instructions based on the input address and provides them to the processor for execution.
 
-## Heng
-Instruction and Data Memory
+### Key Features
+* **Parameterized Design**: The module allows for flexible memory size and data width settings.
+* **Memory Initialization**: The ROM is preloaded with a set of instructions from an external memory file.
+* **Instruction Fetch**: On receiving a read instruction signal, the module fetches the instruction at the given address.
 
-## Jessy
-Register File
-ALU
+## Memory File
+* The memory file (`instruction.mem`) contains the preloaded instructions for the processor.
+* Each line in the file represents a 32-bit instruction in hexadecimal format, as per the RV32I instruction set.
+
+### Instructions Overview
+* The instructions in the file are a subset of the RISC-V RV32I instruction set.
+* Examples include:
+  - **ADD**: Adds the contents of two registers and stores the result in a destination register.
+  - **SLL**: Performs a logical left shift on a register value.
+  - **SLT**: Sets the destination register to 1 if the first source register is less than the second (signed comparison).
+  - **SLTU**: Unsigned version of SLT.
+  - **XOR**: Performs a bitwise exclusive OR between two registers.
+  - **OR**: Performs a bitwise OR between two registers.
+  - **AND**: Performs a bitwise AND between two registers.
+  - **ADDI**: Adds an immediate value to a register.
+  - **BEQ**: Branches to an address if two registers are equal.
+  - **ECALL/EBREAK**: System call or breakpoint instructions for special operations.
+
+
+# Data Memory
+
+
+## Module Implementation
+* The `Data` module is a System Verilog implementation designed to emulate the processor's data memory.
+* It incorporates multiple memory arrays and supports various modes of operation for both reading and writing data.
+
+### Key Features
+* **Multiple Memory Arrays**: Comprises four separate arrays (`dmem1`, `dmem2`, `dmem3`, `dmem4`), each 8-bit wide and 1024 elements long.
+* **Memory Initialization**: On module startup, all memory arrays are initialized to zero.
+* **Flexible Addressing**: Addressing logic allows for byte-aligned access to 32-bit wide memory, accommodating different read and write operations.
+
+## Data Access and Manipulation
+* **Address Calculation**: 
+  - The `addr_in` input is masked and adjusted to calculate the actual memory address (`addr_dmem`) for data access.
+  - Addresses are further processed to determine the specific row and byte index within the memory arrays.
+* **Read Operations**:
+  - The module supports reading data in various sizes (byte, half-word, word) based on the `r_mode` signal.
+  - Data read from the memory arrays is combined and outputted through `dout`.
+* **Write Operations**:
+  - Write operations are contingent on the `w_mode` signal.
+  - Data (`din`) can be written in different sizes to the memory arrays depending on the operational mode and the address.
+
+## Testbench (`Testbench_DM`)
+* The testbench is designed to extensively verify the module's functionality under various scenarios.
+* It includes a clock generation process and simulates different read/write operations.
+
+### Testing Scenarios
+* **Initial Conditions**: Verifies the memory's initial state post-reset.
+* **Byte, Half-Word, and Word Access**: Tests reading and writing of different data sizes.
+* **Boundary Cases**: Assesses module behavior at the edges of the memory space.
+* **Failure Checks**: Includes assertions to confirm the module's correct response to various inputs and conditions.
